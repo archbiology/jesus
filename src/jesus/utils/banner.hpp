e4c7093ec144b1ecc744a0d4e97a621ba50b1fa3
Ruben Alves | Jesus Loves You <rubenanapu@hotmail.com>
2025-08-19 09:31:31 -0300

    Removed tracked version.hpp and replaced Jesus with Jesus Christ on banner

--- src/jesus/utils/banner.hpp
+++ src/jesus/utils/banner.hpp
@@ -7,8 +7,8 @@ namespace Banner
         if (quiet)
             return;
 
-        std::cout << "Jesus Programming Language " << version << " (י ש ו ע ה, " << commit << ")\n\n";
-        std::cout << "\"If you confess with your mouth that 'Jesus is Lord', \n";
+        std::cout << "Jesus Programming Language " << version << " (י ש ו ע ה) [" << commit << "]\n\n";
+        std::cout << "\"If you confess with your mouth that 'Jesus Christ is Lord', \n";
         std::cout << "and believe in your heart that God raised him from the dead,\n";
         std::cout << "you will be saved.\" — Romans 10:9\n";
         // std::cout << "\"May the peace of the Lord Jesus Christ, the Prince of Peace, rest upon you.\"\n";
