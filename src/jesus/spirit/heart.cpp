a7d5db8270e3bb44dd80976084464b7fd106a863
Ruben Alves | Jesus Loves You <rubenanapu@hotmail.com>
2025-08-30 21:29:14 -0300

    feat: Embeding SpiritOfUnderstanding into Heart (scope) so that we have scoped Semantics Analyzer
    "Turn your ear to wisdom and apply your heart to understanding." — Proverbs 2:2

--- src/jesus/spirit/heart.cpp
+++ src/jesus/spirit/heart.cpp
@@ -1,14 +1,15 @@
 #include "heart.hpp"
 #include <stdexcept>
 
-void Heart::createVar(const std::string &name, const Value &value)
+void Heart::createVar(const std::string &type, const std::string &name, const Value &value)
 {
     if (varExists(name))
     {
-        throw std::runtime_error("The variable '" + name + "' already exists.");
+        throw std::runtime_error("The variable '" + name + "' already exists" + " (scope: " + scope_name + ").");
     }
 
     variables[name] = value;
+    registerVarType(type, name);
 }
 
 Value Heart::getVar(const std::string &name) const
@@ -16,7 +17,7 @@ Value Heart::getVar(const std::string &name) const
     auto it = variables.find(name);
     if (it == variables.end())
     {
-        throw std::runtime_error("Undefined variable: " + name);
+        throw std::runtime_error("Undefined variable: " + name + " (scope: " + scope_name + ")");
     }
 
     return it->second;
@@ -26,7 +27,7 @@ void Heart::updateVar(const std::string &name, const Value &value)
 {
     if (!varExists(name))
     {
-        throw std::runtime_error("Cannot assign to undefined variable: " + name);
+        throw std::runtime_error("Cannot assign to undefined variable: " + name + " (scope: " + scope_name + ")");
     }
 
     variables[name] = value;
