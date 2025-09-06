17c8b39d7a77bf560fa0d42b2d66c53d1a61205f
Ruben Alves | Jesus Loves You <rubenanapu@hotmail.com>
2025-09-06 15:26:42 -0300

    feat(interpreter): Methods now know their params also at runtime (not only at parse time).
    "In their hearts humans plan their course, but the LORD establishes their steps." — Proverbs 16:9

--- src/jesus/interpreter/runtime/method.cpp
+++ src/jesus/interpreter/runtime/method.cpp
@@ -5,10 +5,19 @@
 Value Method::call(Interpreter &interpreter, std::shared_ptr<Instance> instance, const std::vector<Value> args)
 {
 
+    // 1. Create a new runtime scope (frame) for this call
+    auto paramsScope = params->clone("call-" + name);
+
+    // 2. Push scope
+    interpreter.addScope(paramsScope);
+
     for (auto stmt : body)
     {
         interpreter.execute(stmt);
     }
+
+    // 3. Pop scope
+    interpreter.popScope();
 
     return Value::formless(); // default return
 }
